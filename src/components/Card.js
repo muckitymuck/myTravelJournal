361ac27851e4df11f5d81ad507e1f106ce868faf
andre <andrewgoldstein@hotmail.com>
2022-01-11 05:09:13 -0600

    initialized

---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function Card(props) {
+    
+    return (
+        <div className="card">
+            <img className="card--image" src={props.item.imageUrl} />
+            <div className="card--info">
+                <div className="card--location">
+                    <span><img className="mappoint" src="./images/pngwing.com.png" />  <p className="card--country">{props.item.location}  <a href={props.item.googleMapsUrl} className="card--googlemaps">View On Google Maps</a></p></span>
+                </div>
+                <div className="card--title">
+                    <span>{props.item.title}</span>
+                </div>
+                <div className="card--stats">
+                    <p><span className="card--date-range">{props.item.startDate} - {props.item.endDate}</span></p>
+                </div>
+                <span><p className="card-description">{props.item.description}</p></span>
+            </div>
+            
+        </div>
+    )
+    
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Navbar(props) {
+    
+    return (
+        <div className="navbar">
+            <h4 className="navbar--title">my travel jounal</h4>
+        </div>
+        
+    )     
+        
+        
+    
+    
+    }
+    
